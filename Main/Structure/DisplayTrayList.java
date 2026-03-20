51a2cc42d05ef5c7c92854a885b69854336abf98
sarun <sarunas10boom@gmail.com>
2026-03-20 12:30:16 +0000

    rest of lists done remove add size and gethead done

--- Main/Structure/DisplayTrayList.java
+++ Main/Structure/DisplayTrayList.java
@@ -3,5 +3,44 @@ package Structure;
 public class DisplayTrayList {
     private Node<DisplayTray> head;
     private int size;
-    // add, remove, size, getHead...
+
+    public void add(DisplayTray displayTray) {
+        Node<DisplayTray> newNode = new Node<>(displayTray);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<DisplayTray> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+
+    }
+    public boolean remove(DisplayTray target) {
+        if (head == null) return false;
+        if (head.data == target) {
+            head = head.next;
+            size--;
+            return true;
+        }
+        Node<DisplayTray> current = head;
+        while (current.next != null && current.next.data != target) {
+            current = current.next;
+        }
+        if (current.next == null) return false;
+        current.next = current.next.next;
+        size--;
+        return true;
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Node<DisplayTray> getHead() {
+        return head;
+    }
 }
